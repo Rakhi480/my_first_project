ec6a46b46e87df6f94d746ca1f3bd0db3d7ccf96
Rakhi480 <147369166+Rakhi480@users.noreply.github.com>
2023-10-09 16:27:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  // Read the content of the index.html file
+  const filePath = path.join(__dirname, 'index.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
 });
 
 const port = 3000;
@@ -11,4 +22,3 @@ const port = 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
